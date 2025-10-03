22ab2f7794bdc39ab592342e18f3d0ba0023a3cf
pjconnect <pasindu.connect@gmail.com>
2025-10-03 18:32:10 +0530

    fix root

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/bitcoinnu-website">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/whitepaper" element={<WhitePaper />} />
